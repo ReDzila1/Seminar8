f427f445c83509250789211daf03fc1c8517ce54
ReDzila1 <137879249+ReDzila1@users.noreply.github.com>
2023-06-27 16:50:36 +0500

    Add files via upload

--- Example1/Program.cs
+++ Example1/Program.cs
@@ -1,5 +1,25 @@
-﻿// Какое из чисел больше всего
-int a = 5;
-int b = 6;
-if (a > b) Console.WriteLine(a);
-else Console.WriteLine(b);
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 121;
+int b1 = 72;
+int c1 = 443;
+
+int a2 = 47;
+int b2 = 92;
+int c2 = 258; 
+
+int a3 = 1102;
+int b3 = 22;
+int c3 = 1;
+
+int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
+
+Console.WriteLine(Max(max1, max2, max3));
