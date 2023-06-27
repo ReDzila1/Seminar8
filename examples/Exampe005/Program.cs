78554be8fa2ce7e5a9ac4bfeff60ce95d83369bd
ReDzila <TopTrader9999@gmail.com>
2023-06-27 14:42:58 +0500

    Masha

---
+++ examples/Exampe005/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Скажите ваше имя");
+string a = Console.ReadLine();
+if (a == "Маша")
+{
+    Console.WriteLine("Здравствуй, Машенька!");
+}
+else
+{
+    Console.WriteLine("Привет, " + a);
+}
