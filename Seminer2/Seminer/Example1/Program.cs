8cbef9ea7a563f7ccd7663082cd1b31f43977def
ReDzila <TopTrader9999@gmail.com>
2023-06-28 13:19:06 +0500

    1 задача

--- Seminer2/Seminer/Example1/Program.cs
+++ Seminer2/Seminer/Example1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >=100 && number <1000)
+{
+    int mid = number % 100;
+    int mid2 = number % 10;
+    int count = mid - mid2;
+    count = count / 10;
+    System.Console.WriteLine($"Вторая цифра числа " + number + " является " + count);
+}
+else
+{
+    System.Console.WriteLine("Это число не трехзначное"); 
+}  
