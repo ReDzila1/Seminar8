c0a17033c4d4aa981f50d11d13dc5615957e8736
ReDzila <TopTrader9999@gmail.com>
2023-07-13 21:25:19 +0500

    1 задание

---
+++ Seminer3/Examples/Domashnee1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Палиндром или нет
+
+Console.WriteLine("Введите число: ");
+string A = Console.ReadLine();
+int len = A.Length;
+
+if (len == 5)
+{
+    if (A[0] == A[4] && A[1] == A[3])
+    {
+        Console.WriteLine("Число палиндром");
+    }
+    else
+    {
+        Console.WriteLine("Число не палиндром");
+    }
+}
+else
+{
+    Console.WriteLine("Число не пятизначное");
+}
