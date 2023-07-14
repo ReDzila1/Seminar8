86f0f9784044a8e78f446dd7b5925a77dc925b28
ReDzila <TopTrader9999@gmail.com>
2023-07-14 22:49:18 +0500

    main

---
+++ Seminer6/Examples/Domashnee1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.WriteLine("Сколько чисел вы хотите ввести?");
+int K = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите числа");
+int[] array = new int[K];
+int G = 0;
+void Vst(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = int.Parse(Console.ReadLine()!);
+        if (array[i] > 0) G += 1;
+    }
+}
+
+Vst(array);
+Console.WriteLine("Кол-во положительных чиcел = " + G);
---
+++ Seminer6/Examples/Domashnee2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.WriteLine("Задайте сначала k1, а затем b1");
+double k1 = double.Parse(Console.ReadLine()!);
+double b1 = double.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Задайте сначала k2, а затем b2");
+double k2 = double.Parse(Console.ReadLine()!);
+double b2 = double.Parse(Console.ReadLine()!);
+
+void fun(double k1, double k2, double b1, double b2){
+    if (k1 == k2) 
+    {
+        Console.WriteLine("Прямые параллельны");
+    }
+    else
+    {
+        double x = (b2 -b1) / (k1 - k2);
+        double y = k1 * x + b1;
+        Console.WriteLine($"Точка пересения двух прямых - [{x};{y}]");
+    }
+}
+
+fun(k1, k2, b1, b2);
