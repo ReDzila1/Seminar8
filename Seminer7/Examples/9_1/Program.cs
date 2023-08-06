0d9954ae61ce2bda02d5db87fccda7177fae0b82
ReDzila <TopTrader9999@gmail.com>
2023-08-06 14:32:49 +0500

    A

---
+++ Seminer7/Examples/9_1/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+void Mass(int M, int N)
+{
+    if (M != N)
+    {
+        if (M % 2 == 0) 
+        {
+            Console.WriteLine(M);
+            Console.WriteLine("-");
+            Mass(M + 1, N);
+        }
+        else Mass(M + 1, N);
+    }
+}
+Console.WriteLine("Введите число M");
+int r = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число N");
+int c = int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+if(r < c)
+{
+    Console.WriteLine($"Четные числа в диапозоне от {r} до {c}:");
+    Mass(r, c);
+}
+else Console.WriteLine("Первое число должно быть меньше второго");
---
+++ Seminer7/Examples/9_2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+void Mass(int M, int N, int L)
+{
+    if (M != N)
+    {
+        Mass(M + 1, N, L + M);
+    }
+    else Console.WriteLine(L + M);
+}
+Console.WriteLine("Введите число M");
+int r = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число N");
+int c = int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+int l = 0;
+if(r < c)
+{
+    Console.WriteLine($"Сумма чисел от {r} до {c}");
+    Mass(r, c, l);
+}
+else Console.WriteLine("Первое число должно быть меньше второго");
